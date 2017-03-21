41d23a810ab02808c784e1b1daef8cf4cbaab1f5
Denis Tatina <sined@sined.desktop>
2017-03-21 18:58:02 +0100

    Angualr Material initial config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     HttpModule,
     AppRoutingModule,
+    MaterialModule
   ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ],
